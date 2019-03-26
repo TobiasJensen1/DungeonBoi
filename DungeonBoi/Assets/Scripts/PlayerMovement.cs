3c561877105d294bb27e067e95fa99bc2004cfeb
tobbe <tobbejj@hotmail.com>
2019-03-26 15:25:14 +0100

    m

---
+++ DungeonBoi/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Vector2 pushForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pushForce = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        move();
+    }
+
+
+    void move()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pushForce, 3*Time.deltaTime);
+        }
+    }
+}
---
+++ DungeonBoi/Assets/Scripts/ProcedualGeneration.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class ProcedualGeneration : MonoBehaviour
+{
+    public GameObject startChunk;
+    public GameObject chunk1;
+    bool hasEast = false;
+    bool hasWest = false;
+
+    bool hasNorth = false;
+    bool hasSouth = false;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject objectToPlace = chunk1;
+
+
+        if(collision.transform.tag == "Player")
+        {
+            if(transform.gameObject.name == "ColliderEast" && !hasEast) { 
+            Instantiate(chunk1, new Vector2(startChunk.transform.position.x + 14, startChunk.transform.position.y), Quaternion.identity);
+            Camera.main.transform.DOMoveX(startChunk.transform.position.x+15, 2);
+                hasEast = true;
+
+            }
+            if (transform.gameObject.name == "ColliderWest" && !hasWest)
+            {
+                Instantiate(chunk1, new Vector2(startChunk.transform.position.x - 14, startChunk.transform.position.y), Quaternion.identity);
+                Camera.main.transform.DOMoveX(startChunk.transform.position.x -15, 2);
+                hasWest = true;
+            }
+            
+        }
+    }
+}
