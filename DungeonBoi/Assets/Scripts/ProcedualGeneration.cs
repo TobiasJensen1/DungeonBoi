03075dec13e52c11b9406af921b0f5d21dda9921
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-03-27 17:35:10 +0100

    d

--- DungeonBoi/Assets/Scripts/ProcedualGeneration.cs
+++ DungeonBoi/Assets/Scripts/ProcedualGeneration.cs
@@ -6,28 +6,57 @@ using DG.Tweening;
 
 public class ProcedualGeneration : MonoBehaviour
 {
-    GameObject test;
+    GameObject chunk_to_spawn;
 
     float Randomizer_Enemy;
+    int chunkConnecter_rng;
 
     Vector2 Enemy_spawner;
-    
+
+    Vector3 camera_Pos;
 
     Vector2 chunkChecker;
 
     public GameObject spider;
+    //Chunks
     public GameObject startChunk;
-    public GameObject chunk1;
-   static bool spawn = false;
-   static List<Vector2> chunks = new List<Vector2>();
+    public GameObject ConnecterChunk1;
+    public GameObject ConnecterChunk2;
+    public GameObject ConnecterChunk3;
+
+    //one way chunks
+    public GameObject EastWestChunk1;
+    public GameObject EastWestChunk2;
+    public GameObject NorthSouthChunk1;
+
+
+    static bool spawn = false;
+   static List<Vector2> ConnecterChunks = new List<Vector2>();
+     List<GameObject> chunkAmount = new List<GameObject>();
 
 
    
     // Start is called before the first frame update
     void Start()
     {
-
-        chunks.Add(startChunk.transform.position);
+        if (transform.name == "ColliderEast" || transform.name == "ColliderWest")
+        {
+            chunkAmount.Add(ConnecterChunk1);
+            chunkAmount.Add(ConnecterChunk2);
+            chunkAmount.Add(ConnecterChunk3);
+            chunkAmount.Add(EastWestChunk1);
+            chunkAmount.Add(EastWestChunk2);
+           
+        }
+        if (transform.name == "ColliderNorth" || transform.name == "ColliderSouth")
+        {
+            chunkAmount.Add(ConnecterChunk1);
+            chunkAmount.Add(ConnecterChunk2);
+            chunkAmount.Add(ConnecterChunk3);
+            chunkAmount.Add(NorthSouthChunk1);
+           
+        }
+        ConnecterChunks.Add(startChunk.transform.position);
     }
 
     // Update is called once per frame
@@ -38,7 +67,8 @@ public class ProcedualGeneration : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameObject objectToPlace = chunk1;
+       
+        GameObject objectToPlace = ConnecterChunk1;
 
         
    
@@ -46,62 +76,35 @@ public class ProcedualGeneration : MonoBehaviour
         if(collision.transform.tag == "Player")
         {
             if(transform.gameObject.name == "ColliderEast" && !spawn) {
-              test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x + 17, startChunk.transform.position.y), Quaternion.identity);
-               if(test.tag == "4Way")
-                {
-                    for (int i = 0; i < 2; i++) {
-                        Randomizer_Enemy = Random.Range(0, 2);
-                        Enemy_spawner = test.transform.Find("EnemySpawner").GetChild(i).position;
-                        print(Randomizer_Enemy);
-                       if(Randomizer_Enemy == 1)
-                        {
-                            Instantiate(spider, new Vector2(Enemy_spawner.x, Enemy_spawner.y), Quaternion.identity);
-                        }
-                        print(Enemy_spawner);
-                            }
-                  
-                }
-                chunks.Add(test.transform.position);
-               
-            Camera.main.transform.DOMoveX(startChunk.transform.position.x+18, 2);
-                spawn = true;
+
+                MultiWay_Chunk_Spawner();
             }
             
             if (transform.gameObject.name == "ColliderEast" && spawn && collision.transform.position.x < transform.position.x)
             {
-                Camera.main.transform.DOMoveX(startChunk.transform.position.x + 18, 2);
 
-               chunkChecker = new Vector2(startChunk.transform.position.x + 17, startChunk.transform.position.y);
+                
+              Camera.main.transform.DOMoveX(startChunk.transform.position.x + 17, 2);
+                
+
+                chunkChecker = new Vector2(startChunk.transform.position.x + 17, startChunk.transform.position.y);
 
 
-                if (!chunks.Contains(chunkChecker))
+                if (!ConnecterChunks.Contains(chunkChecker))
                 {
                     print("kun 1 gang");
-                    test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x + 17, startChunk.transform.position.y), Quaternion.identity);
+                    chunk_to_spawn = Instantiate(ConnecterChunk1, new Vector2(startChunk.transform.position.x + 17, startChunk.transform.position.y), Quaternion.identity);
+                  
 
-                    if (test.tag == "4Way")
-                    {
-                        
-                        for (int i = 0; i < test.transform.Find("EnemySpawner").transform.childCount; i++)
-                        {
-                            Randomizer_Enemy = Random.Range(0, 2);
-                            Enemy_spawner = test.transform.Find("EnemySpawner").GetChild(i).position;
-                            print(Randomizer_Enemy);
-                            if (Randomizer_Enemy == 1)
-                            {
-                                Instantiate(spider, new Vector2(Enemy_spawner.x, Enemy_spawner.y), Quaternion.identity);
-                            }
-                            print(Enemy_spawner);
-                        }
-                        
-                    }
+                    //rng for spawn af enemies
+                   // enemeySpawner();
 
 
-                    chunks.Add(test.transform.position);
+                    ConnecterChunks.Add(chunk_to_spawn.transform.position);
                 }
-                if (chunks.Contains(chunkChecker))
+                if (ConnecterChunks.Contains(chunkChecker))
                 {
-                    print("nope");
+                    
                   
                 }
 
@@ -110,27 +113,29 @@ public class ProcedualGeneration : MonoBehaviour
             
             if (transform.gameObject.name == "ColliderWest" && !spawn)
             {
-          test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x - 17, startChunk.transform.position.y), Quaternion.identity);
+                chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
+                chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x - 17, startChunk.transform.position.y), Quaternion.identity);
 
-                chunks.Add(test.transform.position);
+                ConnecterChunks.Add(chunk_to_spawn.transform.position);
                 
 
-                Camera.main.transform.DOMoveX(startChunk.transform.position.x -18, 2);
+                Camera.main.transform.DOMoveX(startChunk.transform.position.x -17, 2);
                 spawn = true;
             }
             if (transform.gameObject.name == "ColliderWest" && spawn && collision.transform.position.x > transform.position.x)
             {
-                Camera.main.transform.DOMoveX(startChunk.transform.position.x - 18, 2);
+                Camera.main.transform.DOMoveX(startChunk.transform.position.x - 17, 2);
 
                 chunkChecker = new Vector2(startChunk.transform.position.x - 17, startChunk.transform.position.y);
 
-                if (!chunks.Contains(chunkChecker))
+                if (!ConnecterChunks.Contains(chunkChecker))
                 {
+                    chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
                     print("kun 1 gang");
-                    test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x - 17, startChunk.transform.position.y), Quaternion.identity);
-                    chunks.Add(test.transform.position);
+                    chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x - 17, startChunk.transform.position.y), Quaternion.identity);
+                    ConnecterChunks.Add(chunk_to_spawn.transform.position);
                 }
-                if (chunks.Contains(chunkChecker))
+                if (ConnecterChunks.Contains(chunkChecker))
                 {
                     print("nope");
 
@@ -141,9 +146,10 @@ public class ProcedualGeneration : MonoBehaviour
 
             if (transform.gameObject.name == "ColliderSouth" && !spawn)
             {
-                test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y - 13), Quaternion.identity);
+                chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
+                chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y - 13), Quaternion.identity);
 
-                chunks.Add(test.transform.position);
+                ConnecterChunks.Add(chunk_to_spawn.transform.position);
 
 
                 Camera.main.transform.DOMoveY(startChunk.transform.position.y - 14, 2);
@@ -155,13 +161,14 @@ public class ProcedualGeneration : MonoBehaviour
 
                 chunkChecker = new Vector2(startChunk.transform.position.x, startChunk.transform.position.y - 13);
 
-                if (!chunks.Contains(chunkChecker))
+                if (!ConnecterChunks.Contains(chunkChecker))
                 {
+                    chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
                     print("kun 1 gang");
-                    test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y - 13), Quaternion.identity);
-                    chunks.Add(test.transform.position);
+                    chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y - 13), Quaternion.identity);
+                    ConnecterChunks.Add(chunk_to_spawn.transform.position);
                 }
-                if (chunks.Contains(chunkChecker))
+                if (ConnecterChunks.Contains(chunkChecker))
                 {
                     print("nope");
 
@@ -170,9 +177,10 @@ public class ProcedualGeneration : MonoBehaviour
             }
             if (transform.gameObject.name == "ColliderNorth" && !spawn)
             {
-                test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y + 13), Quaternion.identity);
+                chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
+                chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y + 13), Quaternion.identity);
 
-                chunks.Add(test.transform.position);
+                ConnecterChunks.Add(chunk_to_spawn.transform.position);
 
 
                 Camera.main.transform.DOMoveY(startChunk.transform.position.y + 12, 2);
@@ -180,17 +188,18 @@ public class ProcedualGeneration : MonoBehaviour
             }
             if (transform.gameObject.name == "ColliderNorth" && spawn && collision.transform.position.y < transform.position.y)
             {
+                chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
                 Camera.main.transform.DOMoveY(startChunk.transform.position.y + 12, 2);
 
                 chunkChecker = new Vector2(startChunk.transform.position.x, startChunk.transform.position.y + 13);
 
-                if (!chunks.Contains(chunkChecker))
+                if (!ConnecterChunks.Contains(chunkChecker))
                 {
                     print("kun 1 gang");
-                    test = Instantiate(chunk1, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y + 13), Quaternion.identity);
-                    chunks.Add(test.transform.position);
+                    chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x, startChunk.transform.position.y + 13), Quaternion.identity);
+                    ConnecterChunks.Add(chunk_to_spawn.transform.position);
                 }
-                if (chunks.Contains(chunkChecker))
+                if (ConnecterChunks.Contains(chunkChecker))
                 {
                     print("nope");
 
@@ -202,6 +211,40 @@ public class ProcedualGeneration : MonoBehaviour
         
     }
 
-    
+    public void enemeySpawner()
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            Randomizer_Enemy = Random.Range(0, 2);
+            Enemy_spawner = chunk_to_spawn.transform.Find("EnemySpawner").GetChild(i).position;
+            print(Randomizer_Enemy);
+            if (Randomizer_Enemy == 1)
+            {
+                Instantiate(spider, new Vector2(Enemy_spawner.x, Enemy_spawner.y), Quaternion.identity);
+            }
+            print(Enemy_spawner);
+        }
+    }
+
+    public void MultiWay_Chunk_Spawner()
+    {
+        chunkConnecter_rng = Random.Range(0, chunkAmount.Count);
+
+        chunk_to_spawn = Instantiate(chunkAmount[chunkConnecter_rng].gameObject, new Vector2(startChunk.transform.position.x + 17, startChunk.transform.position.y), Quaternion.identity);
+
+
+     //   enemeySpawner();
+
+        ConnecterChunks.Add(chunk_to_spawn.transform.position);
+
+
+        camera_Pos = chunk_to_spawn.transform.Find("CameraPoint").transform.position;
+
+        Camera.main.transform.DOMoveX(camera_Pos.x, 2);
+        spawn = true;
+    } 
    
+    
+    
+
 }
