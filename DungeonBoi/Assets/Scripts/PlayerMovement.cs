f0f2bee8f2ddc5ec1f1ff14fedf3c9d56529c984
tobbe <tobbejj@hotmail.com>
2019-04-01 15:59:35 +0200

    broken combat, mangler noget collision

--- DungeonBoi/Assets/Scripts/PlayerMovement.cs
+++ DungeonBoi/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
     Vector2 pushForce;
     public float speed;
 
+    float distanceX;
+    float distanceY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     {
         pushForce = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         move();
+        attack();
         
     }
 
@@ -30,7 +34,27 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(transform.position, pushForce, speed*Time.deltaTime);
         }
+        
+
     }
+
+    void attack() { 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        if (hit.collider != null && hit.collider.transform.tag == "Enemy")
+        {
+            distanceX = transform.position.x - hit.collider.transform.position.x;
+            distanceY = transform.position.y - hit.collider.transform.position.y;
+
+            if (Input.GetMouseButton(0) && distanceX <= 0.3 && distanceX <= 0.3 ) { 
+            Destroy(hit.collider.gameObject);
+                print("attack!");
+            }
+
+
+        }
+    }
+    
 
 
 
